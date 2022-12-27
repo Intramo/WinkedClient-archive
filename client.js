a0ba4811dc09e2f62129b54df8f8239927f5d0ca
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2022-12-27 13:55:47 +0100

    Update client

---
+++ client.js
@@ -0,0 +1,165 @@
+console.log("#### ##    ## ######## ########     ###    ##     ##  #######  \n##  ###   ##    ##    ##     ##   ## ##   ###   ### ##     ## \n##  ####  ##    ##    ##     ##  ##   ##  #### #### ##     ## \n##  ## ## ##    ##    ########  ##     ## ## ### ## ##     ## \n##  ##  ####    ##    ##   ##   ######### ##     ## ##     ## \n##  ##   ###    ##    ##    ##  ##     ## ##     ## ##     ## \n#### ##    ##    ##    ##     ## ##     ## ##     ##  #######  ")
+
+var gameState = "playerJoin" // playerJoin, waiting, answerNormal, answerTrueFalse, playerCorrect, playerWrong, hostLobby, hostPodium, hostLeaderboard, hostResultsNormal, hostResultsTrueFalse, hostAnswers, hostQuestion
+var playerAmount = 0
+
+refreshDisplay();
+
+function startCountDownByWordLength(length) {
+    startCountdown(length * 160)
+}
+
+function startCountdown(length) {
+    countdownStart = Date.now()
+    countdownDuration = length
+}
+
+function getCountdown() {
+    if (Date.now() - countdownStart > countdownDuration) { return 0 }
+    return (countdownDuration + (countdownStart - Date.now())) / 1000
+}
+
+function onButtonPress(btn) { // A, B, C, D, Y, N
+    gameState = "waiting"
+    refreshDisplay();
+    connection.send(JSON.stringify({ "packettype": "answer", "button": btn }))
+}
+
+function next() {
+    countdownStart = 2147483645 * 1000
+    connection.send(JSON.stringify({ "packettype": "next" }))
+}
+
+function onLogin() {
+    let pin = document.getElementById('page-playerJoin-id').value.trim();
+    let name = document.getElementById('page-playerJoin-name').value.trim();
+
+    if(pin === ""){
+        alert("Das Feld für die Spiel-ID ist leer")
+        return
+    }
+
+    if(name === ""){
+        alert("Das Feld für deinen Namen ist leer")
+        return
+    }
+
+    if(name.length < 3){
+        alert("Dein Name muss mindestens 3 Zeichen haben")
+        return
+    }
+
+    connection.send(JSON.stringify({ "packettype": "joinRequest", "session": pin, "name": name }))
+}
+
+function refreshDisplay() {
+    for (const element of document.getElementsByClassName("page")) {
+        element.style.display = "none";
+    }
+    document.getElementById("page-" + gameState).style.display = "block";
+}
+
+var connection = new WebSocket("ws://localhost:4348/");
+
+connection.onopen = function (e) {
+    console.log("Verbindung zu den Servern hergestellt")
+};
+
+connection.onclose = function (event) {
+    alert('Verbindung abgebrochen');
+};
+
+connection.onerror = function (error) {
+    alert('Verbindungsfehler');
+};
+
+connection.onmessage = function (event) {
+    let data = JSON.parse(event.data)
+
+    if (data["packettype"] === "error") {
+        alert(data["message"])
+        return
+    }
+
+    if (data["packettype"] === "lobbyJoin") {
+        playerAmount += 1
+        document.getElementById("pageHostLobbyPlayerList").innerHTML = document.getElementById("pageHostLobbyPlayerList").innerHTML + "<p>" + data["name"] + "</p>"
+        return
+    }
+
+    if (data["packettype"] === "addAnswerCount") {
+        //TODO
+        return
+    }
+
+    if (data["packettype"] === "gamestate") {
+        gameState = data["gameState"]
+
+        /*
+        var-gameID
+        var-playerAmount
+        var-points
+        var-progress
+        var-answerstreak
+        var-question
+        var-answerA
+        var-answerB
+        var-answerC
+        var-answerD
+        var-answerAAmount
+        var-answerBAmount
+        var-answerCAmount
+        var-answerDAmount
+        var-answerCount
+        */
+        
+        if (gameState === "hostLobby"){
+            for (const element of document.getElementsByClassName("var-gameID")) { element.innerHTML = data["gameid"] };
+        }
+
+        if (gameState === "playerAnswerNormal"){
+            for (const element of document.getElementsByClassName("var-points")) { element.innerHTML = data["points"] };
+            for (const element of document.getElementsByClassName("var-progress")) { element.innerHTML = data["progress"] };
+            if(data["buttons"]["A"] === true) {} //TODO: Implement hiding buttons
+            if(data["buttons"]["B"] === true) {} //TODO: Implement hiding buttons
+            if(data["buttons"]["C"] === true) {} //TODO: Implement hiding buttons
+            if(data["buttons"]["D"] === true) {} //TODO: Implement hiding buttons
+        }
+
+        if (gameState === "playerAnswerTrueFalse"){
+            for (const element of document.getElementsByClassName("var-points")) { element.innerHTML = data["points"] };
+            for (const element of document.getElementsByClassName("var-progress")) { element.innerHTML = data["progress"] };
+        }
+
+        if (gameState === "playerResultCorrect"){
+            for (const element of document.getElementsByClassName("var-answerstreak")) { element.innerHTML = data["answerstreak"] };
+        }
+
+        if (gameState === "hostQuestion"){
+            for (const element of document.getElementsByClassName("var-question")) { element.innerHTML = data["question"] };
+        }
+
+        if (gameState === "hostAnswers"){
+            for (const element of document.getElementsByClassName("var-question")) { element.innerHTML = data["question"] };
+            for (const element of document.getElementsByClassName("var-answerA")) { element.innerHTML = data["answers"]["A"] };
+            for (const element of document.getElementsByClassName("var-answerB")) { element.innerHTML = data["answers"]["B"] };
+            for (const element of document.getElementsByClassName("var-answerC")) { element.innerHTML = data["answers"]["C"] };
+            for (const element of document.getElementsByClassName("var-answerD")) { element.innerHTML = data["answers"]["D"] };
+        }
+
+        if (gameState === "hostResults"){
+            for (const element of document.getElementsByClassName("var-question")) { element.innerHTML = data["question"] };
+            for (const element of document.getElementsByClassName("var-answerA")) { element.innerHTML = data["answers"]["A"]["text"] };
+            for (const element of document.getElementsByClassName("var-answerB")) { element.innerHTML = data["answers"]["B"]["text"] };
+            for (const element of document.getElementsByClassName("var-answerC")) { element.innerHTML = data["answers"]["C"]["text"] };
+            for (const element of document.getElementsByClassName("var-answerD")) { element.innerHTML = data["answers"]["D"]["text"] };
+            for (const element of document.getElementsByClassName("var-answerAAmount")) { element.innerHTML = data["answers"]["A"]["amount"] };
+            for (const element of document.getElementsByClassName("var-answerBAmount")) { element.innerHTML = data["answers"]["B"]["amount"] };
+            for (const element of document.getElementsByClassName("var-answerCAmount")) { element.innerHTML = data["answers"]["C"]["amount"] };
+            for (const element of document.getElementsByClassName("var-answerDAmount")) { element.innerHTML = data["answers"]["D"]["amount"] };
+            //TODO: Correct
+        }
+
+        refreshDisplay();
+    }
+}
